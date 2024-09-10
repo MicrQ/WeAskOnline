23a16e4a820ee40e5709e8fe5a03ec2c60f7dfa6
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-09-10 18:04:20 +0100

    (feat): route for handling the root dir

---
+++ api/v1/route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from flask import Blueprint, jsonify
+
+home = Blueprint('home', __name__)
+
+
+@home.route('/api/v1/')
+def index():
+    """\
+    Root directory and first page for the developer to find
+    moveable routes which route to find resources and to develop the frontend
+    
+    Returns:
+        JSON List[Dict] - list of keys and values (hyperlinks) 
+    """
+    home_data = [
+        {"message": "Hello, developer!"},
+        {"login": "http://localhost:5000/api/v1/login"},
+        {"register": "http://localhost:5000/api/register"},
+        {"password_reset": "http://localhost:5000/api/password-reset"},
+        {"user": "http://localhost:5000/api/users/<userid>"}
+        ]
+    return jsonify(home_data), 200
--- app.py
+++ app.py
@@ -3,12 +3,14 @@
 from flask import Flask
 from api.v1.auth import auth
 from models.base import db
+from api.v1.route import home
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///WeAskOnline.db'
 db.init_app(app)
 app.register_blueprint(auth)
+app.register_blueprint(home)
 
 with app.app_context():
     db.create_all()
