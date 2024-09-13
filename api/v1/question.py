d22c78f53ae21fc48736b6e54b5e2154498251b6
AbenetGebre <mail.micrq@proton.me>
2024-09-13 22:30:15 +0300

    post question feature implemented

---
+++ api/v1/question.py
@@ -0,0 +1,68 @@
+""" Question endpoints """
+from os import abort
+from flask import Blueprint, request, jsonify, redirect, abort
+from models.base import db
+from models.base_redis import RedisServer
+from models.question import Question
+from models.tag import Tag
+from models.user import User
+from models.question_tag import QuestionTag
+from datetime import datetime, timezone
+
+
+question = Blueprint('question', __name__)
+
+
+@question.route('/api/v1/questions', methods=['POST'])
+def create_question():
+    """ Handles question creation for logged in user """
+    token = request.cookies.get('api-token')
+    if not token:
+        return redirect('/api/v1/login')
+
+    redis = RedisServer()
+    username = redis.get(token)
+    if username is None:
+        abort(401)
+    user = db.session.query(User).filter_by(
+        username=username.decode('utf-8')).first()
+    if not user:
+        abort(401)
+
+    data = request.get_json()
+    if not data:
+        return jsonify({'Error': 'Invalid JSON data'}), 400
+    title = data.get('title')
+    if not title:
+        return jsonify({'Error': 'Missing title'}), 400
+    body = data.get('body')
+    if not body:
+        return jsonify({'Error': 'Missing body'}), 400
+
+    now = datetime.now(timezone.utc)
+    question = Question(
+        title=title,
+        body=body,
+        user_id=user.id)
+    db.session.add(question)
+    db.session.commit()
+
+    tags = data.get('tags')
+    if tags:
+        for tag in tags:
+            tag_exists = db.session.query(Tag).filter_by(
+                name=tag.lower()).first()
+            new_tag = Tag(tag)
+            if not tag_exists:
+                db.session.add(new_tag)
+                db.session.commit()
+            new_tag = db.session.query(Tag).filter_by(
+                name=new_tag.name).first()
+            question_tag = QuestionTag(question.id, new_tag.id)
+            db.session.add(question_tag)
+            db.session.commit()
+
+        return jsonify({'message': 'Question created'}), 201
+
+
+# @question.route('api/v1/')
--- app.py
+++ app.py
@@ -4,23 +4,39 @@ from flask import Flask
 from api.v1.auth import auth
 from models.base import db
 from api.v1.route import home
+from api.v1.question import question
 from sqlalchemy import text
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 onDevelopment = True
 if onDevelopment:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///WeAskOnline.db'
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = '' # production db uri here
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''  # production db uri here
 db.init_app(app)
 app.register_blueprint(auth)
 app.register_blueprint(home)
+app.register_blueprint(question)
 
 with app.app_context():
     if onDevelopment:
         db.session.execute(text('PRAGMA foreign_keys = ON'))
     db.create_all()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ not found handler """
+    return {"Error": "Not found"}, 404
+
+
+@app.errorhandler(401)
+def not_authorized(error):
+    """ not authorized handler """
+    return {"Error": "Not authorized"}, 401
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
