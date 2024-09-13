0a190a4e2ca2c37cc6be0dae0028d1f46b318938
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-09-13 04:41:51 +0100

    (feat): Redis Connection to Cloud this handles the connection to the redis cloud server where user data would be stored temporarily and for token validation from the user.

---
+++ models/base_redis.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Redis connector which connects to the redis server and handles connection"""
+
+from datetime import timedelta
+import os
+from dotenv import load_dotenv
+import redis
+
+
+load_dotenv()
+
+HOST = os.getenv('HOST')
+PORT = os.getenv('PORT')
+PASSWORD = os.getenv('PASSWORD')
+USERNAME = os.getenv('USERNAME')
+
+
+class RedisServer():
+    def __init__(self):
+        # self.redis_client = redis.Redis(HOST, PORT, password=PASSWORD, username=USERNAME)
+        try:
+            self.redis_client = redis.Redis()
+        except Exception as e:
+            print({"error": e})
+    
+    def get(self, key: str):
+        try:
+            return self.redis_client.get(key)
+        except Exception as e:
+            print("Key not found or expired")
+    
+    def set(self, key: str, value: str):
+        if not key or not value:
+            return "key or value is missing"
+        self.redis_client.set(key, value, timedelta(minutes=5.0))
+        return True
+    
+    def hset(self, user_email, token, user_data):
+        if not user_data or not user_data:
+            return False
+        self.redis_client.hset(user_email, mapping=user_data)
+        self.redis_client.hset(user_email, "token", token)
+        self.redis_client.expire(user_email, 5 * 60)
+        return True
+
+    def hgetall(self, email: str):
+        return self.redis_client.hgetall(email)
