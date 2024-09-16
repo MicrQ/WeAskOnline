541b106f4b84bb9a45835d24a0f9c502cd359107
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-09-17 00:37:59 +0100

    (mod): check cases where the user is already has a vote in the comment table and then check if the data being passed is valid

--- api/v1/vote.py
+++ api/v1/vote.py
@@ -48,14 +48,43 @@ def create_vote(entity, id):
     if not vote:
         return jsonify({'error': 'Missing vote entity'}), 400
 
+    vote = vote.lower()
+
     model = db.session.query(entities.get(entity)).filter_by(id=id).first()
     if not model:
         abort(404)
     
-    user_vote = Vote(
-        is_upvote=True if vote == 1 else False,
-        user_id=user.id, parent_type=entity.lower(),
-        parent_id=id)
-    db.session.add(user_vote)
+    check_user_vote = db.session.query(Vote).filter_by(
+        parent_type=entity.lower(),
+        parent_id=id,
+        user_id=user.id
+        ).first()
+
+    if not check_user_vote:
+        if vote not in ["novote", "upvote", "downvote"]:
+            return jsonify({'error': 'Invalid vote entity'}), 400
+
+        user_vote = Vote(
+            is_upvote=False,
+            user_id=user.id, parent_type=entity.lower(),
+            parent_id=id)
+        
+        # db.session.add(user_vote)
+
+        if vote == 'upvote':
+            user_vote.is_upvote = True
+        db.session.add(user_vote)
+
+    elif check_user_vote:
+        if vote == 'novote':
+            db.session.delete(check_user_vote)
+        elif vote == 'downvote':
+            check_user_vote.is_upvote = False
+        elif vote == 'upvote':
+            check_user_vote.is_upvote = True
+        else:
+            db.session.close()
+            return jsonify({'error': 'Invalid vote entity'}), 400
+
     db.session.commit()
     return jsonify({'message': 'Success'}), 200
