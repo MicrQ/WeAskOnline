a0de493c8001d89c5bc4db66455e1a90a0113a4b
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-09-17 12:55:31 +0100

    (mod): added a route handler for finding the courrence for the questions which has the tag

--- api/v1/tag.py
+++ api/v1/tag.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module which stores the functionality of fetching tags from the DB """
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, abort, jsonify
 from models.question_tag import QuestionTag
 from models.base import db
 from models.question import Question
@@ -24,3 +24,33 @@ def get_tags():
         tag['count'] = db.session.query(QuestionTag).filter_by(tag_id=tag['id']).count()
         new_tag.append(tag)
     return jsonify(new_tag), 200
+
+
+@tag.route('/api/v1/tags/<string:name>')
+def get_tags_with_name(name):
+    """ Fetch questions with tag name passed to parameter
+    passed on the route URL"""
+
+    # Get the tag first and then search for the reoccurence of the tag
+    # from the tag id
+    tag = db.session.query(Tag).filter_by(name=name).first()
+    if not tag:
+        abort(404)
+    
+    # Get the id of the question_tag from the QuestionTag db and then search
+    # for the question based on the found question tag id
+    question_tag = db.session.query(QuestionTag).filter_by(tag_id=tag.id).first()
+    if not question_tag:
+        abort(404)
+    
+    # Get the question based on the id from the QuestionTag table
+    questions = db.session.query(Question).filter_by(id=question_tag.question_id).all()
+    if not questions:
+        abort(404)
+    
+    # loop through the found questions to convert to serializable JSON data
+    question_tag_data = []
+    for question in questions:
+        question_tag_data.append(question.to_dict())
+
+    return jsonify(question_tag_data), 200
