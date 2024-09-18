8efc64a5c2545e7ac603d4427d6dc183eb753ab6
AbenetGebre <mail.micrq@proton.me>
2024-09-18 12:19:33 +0300

    added delete reply

--- api/v1/reply.py
+++ api/v1/reply.py
@@ -95,3 +95,42 @@ def update_reply(comment_id, reply_id):
     reply.isEdited = True
     db.session.commit()
     return jsonify({'message': "Reply updated successfully."}), 200
+
+
+@reply.route('/api/v1/comments/<comment_id>/reply/<reply_id>', methods=["DELETE"])
+def delete_reply(comment_id, reply_id):
+    """ to delete a reply """
+    token = request.cookies.get('api-token')
+    if not token:
+        abort(401)
+
+    redis = RedisServer()
+    if not redis:
+        return jsonify({'Error': 'Redis server not available'}), 500
+
+    username = redis.get(token)
+    if username is None:
+        abort(401)
+    user = db.session.query(User).filter_by(
+        username=username.decode('utf-8')).first()
+    if not user:
+        abort(401)
+
+    comment = db.session.query(Comment).filter_by(id=comment_id).first()
+    if not comment:
+        abort(404)
+
+    reply = db.session.query(Reply).filter_by(
+        id=reply_id, comment_id=comment_id).first()
+    if not reply:
+        abort(404)
+
+    if reply.user_id != user.id:
+        return jsonify(
+            {'Error': 'You don\'t have permission to\
+              delete this reply.'}
+            ), 403
+
+    db.session.delete(reply)
+    db.session.commit()
+    return jsonify({'message': "Reply deleted successfully."}), 200
