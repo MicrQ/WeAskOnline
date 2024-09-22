4c7dab698a641f9e5b56611fe77fe8289f67c3b4
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-09-22 15:26:26 +0100

    (bug-fix): fixed the tab indentation which caused the logic for validating the user credential from the form

--- api/v1/auth.py
+++ api/v1/auth.py
@@ -43,20 +43,20 @@ def login():
         if not user.isActive:
             return jsonify({"error": "User account is inactive"}), 403
 
-            token: str = str(uuid4())
+        token: str = str(uuid4())
 
-            redisConnect = RedisServer()
-            resp = redisConnect.set_token(token, username)
+        redisConnect = RedisServer()
+        resp = redisConnect.set_token(token, username)
 
-            # Check if token was stored in redis server
-            if not resp:
-                return jsonify({"error": "Couldn't  connect to Redis server"}), 500
+        # Check if token was stored in redis server
+        if not resp:
+            return jsonify({"error": "Couldn't  connect to Redis server"}), 500
 
-            # Store the token in the cookie session & expire age at 5 days
-            res = jsonify({"message": "Success", "api-token": token})
-            res.set_cookie('api-token', token, max_age=60 * 60 * 24 * 5)
-            return res, 200
-        else:
+        # Store the token in the cookie session & expire age at 5 days
+        res = jsonify({"message": "Success", "api-token": token})
+        res.set_cookie('api-token', token, max_age=60 * 60 * 24 * 5)
+        return res, 200
+    else:
             return jsonify({"error": "Incorrect password"}), 401
 
 
